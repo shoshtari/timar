88349610ebbebb1e41fc837aefe230381244955f
shoshtari <mr.malekey@gmail.com>
2025-03-27 13:41:07 +0330

    feat: Make timelog repo

---
+++ src/db/time_log_test.py
@@ -0,0 +1,18 @@
+import sqlite3
+from typing import Generator
+
+import pytest
+
+from .time_log import ITimelogRepo, TimelogRepo
+
+
+@pytest.fixture
+def repo() -> Generator[ITimelogRepo, None, None]:
+    conn = sqlite3.connect(":memory:")
+    repo = TimelogRepo(conn, do_migrate=True)
+    yield repo
+    conn.close()
+
+
+def test_timelog_repo(repo: ITimelogRepo) -> None:
+    pass
